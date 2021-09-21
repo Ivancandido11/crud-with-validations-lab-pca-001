0152a3939a0c740567196150f88e01588b1ff07f
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-21 16:57:54 -0400

    Completed Assignment

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,52 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = find_song
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(song_params(:title, :released, :release_year, :artist_name, :genre))
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render("new")
+    end
+  end
+
+  def edit
+    @song = find_song
+  end
+
+  def update
+    @song = find_song
+    @song.update(song_params(:title, :released, :release_year, :artist_name, :genre))
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render("edit")
+    end
+  end
+
+  def destroy
+    @song = find_song
+    @song.destroy
+    redirect_to songs_path
+  end
+
+private
+
+  def find_song
+    Song.find(params[:id])
+  end
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+end
